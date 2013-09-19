9fd520b92aadfa3f6b1879f8b3708e4398c0eac0
Andrew Mukiza <mandy.rgf@gmail.com>
2013-09-19 21:47:30 +0300

    removing other cookbooks apart from memcached, after consolidating them into mics cookbook

--- cookbooks/git/spec/default_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 'git::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'git::default' }
-  it 'should do something' do
-    expect(chef_run).to install_package 'git'
-  end
-end
